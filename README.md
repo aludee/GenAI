ebf1712638823a936043a9427b96502373674f9e
aludee <wa3620015@gmail.com>
2025-05-04 15:46:08 +0530

    Enhance README and main.py: Added resource details, improved company name validation, and ensured API key presence.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # GenAI Projects
 Projects:
     1. Cold Email Generator(Simple)
+        Resources: the required files
+        Utils: the required helper packages
+        Code Structure:
             i. langChain to chain the commands
             ii. GroqCloud to host the Llama moded
             iii. Vector Database(Qdrant)
-            iv. Send the email
+            iv. Send the email  
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 from utils import getClient
 
 import pandas as pd
@@ -16,3 +17,47 @@ for _, row in df.iterrows():
 
 print(docs)
 print(metadatas)
+
+'''
+import os
+from dotenv import load_dotenv
+load_dotenv("E:\\GenAI_projects\\CEGT_git\\GenAI-1\\.env")
+
+
+# 1. Check input company name is valid or not
+
+
+from app.llm_calls import Chain
+chain = Chain()
+
+def isCompanyNameValid(company_name):
+    if not company_name or company_name.isspace():
+        return False
+    if not company_name.isalnum() and not all(char.isspace() for char in company_name):
+        return False
+    if not chain.checkCompany(company_name):
+        return False
+    return True
+
+
+
+company_name = input("Enter the company name: ")
+
+if not isCompanyNameValid(company_name):
+    print("Invalid company name. Please enter a valid name.")
+    exit(1)
+
+
+
+# 2. Check one engg related job posting in that company
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+if not tavily_api_key:
+    raise ValueError("TAVILY_API_KEY is not set in the .env file")
+
+tavily_search = TavilySearchResults(max_results = 1)
+query = "Software Engineer related to AI or backend related jobs at" + company_name
+_the_job = tavily_search.invoke(query)
+_the_link = _the_job[0]["url"]
+print(_the_link)
